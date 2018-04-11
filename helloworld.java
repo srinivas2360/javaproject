b735d61ba925d3b0c4f4c4900cdc772608b38416
srinivas2360 <38001245+srinivas2360@users.noreply.github.com>
2018-04-11 19:31:33 +0530

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[B[B[B[B[B[B[B[B
